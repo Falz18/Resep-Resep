842f937e3cb1801d5a50967d3457e091d0ca679d
Falz18 <naufalalghani.wahyudi18@gmail.com>
2024-06-06 09:38:15 +0700

    update!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuToggle.addEventListener('click', function () {
+        navigation.classList.toggle('active');
+    });
+
+    window.addEventListener('scroll', function () {
+        if (navigation.classList.contains('active')) {
+            navigation.classList.remove('active');
+        }
+    });
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
     let observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
